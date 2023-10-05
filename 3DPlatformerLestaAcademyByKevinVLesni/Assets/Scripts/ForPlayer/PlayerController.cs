c5001195585059b04f380265a9debc22813f20ff
KevinVLesni <krihagosickmatron@gmail.com>
2023-10-05 15:36:50 +0300

    Add player movement
    ►Добавлено передвижение игрока.

--- 3DPlatformerLestaAcademyByKevinVLesni/Assets/Scripts/ForPlayer/PlayerController.cs
+++ 3DPlatformerLestaAcademyByKevinVLesni/Assets/Scripts/ForPlayer/PlayerController.cs
@@ -2,17 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Speed = 10f;
+    public float JumpForce = 300f;
+
+    // Required tag "Ground" for work
+    private bool _isGrounded;
+    private Rigidbody _rb;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MovementLogic();
+        JumpLogic();
+    }
+
+    private void MovementLogic()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
+
+    private void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector3.up * JumpForce);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpdate(collision, true);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpdate(collision, false);
+    }
+
+    private void IsGroundedUpdate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            _isGrounded = value;
+        }
     }
 }
