7ab50c7344deeee3991f5103cac9697813e92224
Nikita531 <110817057+Nikita531@users.noreply.github.com>
2023-10-10 15:07:59 +0300

    Update

--- 3DPlatformerLestaAcademyByKevinVLesni/Assets/Scripts/UI/LoseGameText.cs
+++ 3DPlatformerLestaAcademyByKevinVLesni/Assets/Scripts/UI/LoseGameText.cs
@@ -4,9 +4,23 @@ using UnityEngine.UI;
 
 public class LoseGameText : MonoBehaviour
 {
+    public GameObject objectToActivate;
     public void RestartLevel()
     {
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
+    }
+    
+    
+        void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DeadZone"))
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        objectToActivate.SetActive(true);
     }
 }
