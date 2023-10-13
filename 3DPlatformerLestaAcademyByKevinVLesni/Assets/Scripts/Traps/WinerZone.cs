29f15b75510cc58a281a16334b4526a2c2741b03
KevinVLesni <krihagosickmatron@gmail.com>
2023-10-13 16:01:47 +0300

    Add WindZone
    ►Добавил ветер с настройкой по коллайдеру и триггеру.

---
+++ 3DPlatformerLestaAcademyByKevinVLesni/Assets/Scripts/Traps/WinerZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinerZone : MonoBehaviour
+{
+    public Vector3 windDirection = new Vector3(1, 0, 0);
+    public float windStrength = 10.0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(windDirection * windStrength);
+        }
+    }
+}
